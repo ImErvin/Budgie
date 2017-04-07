680bca99516edb05a058502af50590177c11d0a7
Ervin Mamutov <erviin@live.ie>
2017-04-07 19:45:34 +0100

    Implemented views

--- Budgie/Views/BudgieInitial.xaml.cs
+++ Budgie/Views/BudgieInitial.xaml.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
-using Budgie.Views;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -14,40 +13,32 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
-namespace Budgie
+namespace Budgie.Views
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class BudgieInitial : Page
     {
-        public MainPage()
+        public BudgieInitial()
         {
             this.InitializeComponent();
         }
 
-        private void letStart_Click(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(BudgieInitial));
-        }
-
-        /*private void welcomeSaveBalance_Click(object sender, RoutedEventArgs e)
+        private void welcomeSaveBalance_Click(object sender, RoutedEventArgs e)
         {
             double balance = 0;
             try
             {
                 balance = double.Parse(welcomeBalance.Text);
                 yourBalance.Text = "" + balance;
-                welcomePage.Visibility = Visibility.Collapsed;
-                mainPage.Visibility = Visibility.Visible;
             }
             catch
             {
                 errorMessage.Text = "Not a double value!";
             }
-            
-        }*/
+        }
     }
 }
